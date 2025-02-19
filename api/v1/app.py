fc302bb8448950aa8bab4bbf1483d2a94f2ecee4
ckchesire <christianck.chesire@gmail.com>
2025-02-19 21:21:42 +0300

    Updated app.py adding missing port to listen to i.e 5000.

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,5 +19,5 @@ if __name__ == "__main__":
    """Main program entry point
    """
     HOST = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = int(os.getenv('HBNB_API_PORT',))
+    PORT = int(os.getenv('HBNB_API_PORT', 5000))
     app.run(host=HOST, port=PORT, threaded=True)
